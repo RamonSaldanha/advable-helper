2aa99f08b6e90e37eb958525268c716406a22cde
Ramon Saldanha <ramonsaldanhaa@gmail.com>
2024-01-16 23:54:49 -0300

    Update processPage.vue and main.css

--- src/content-scripts/processPage.vue
+++ src/content-scripts/processPage.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    {{ textContent }}
-  </div>
+    <div>
+      <form action="" method="post">
+        <label class="adble-label" for="processNumber">Número do processo:</label><br>
+        <input class="adble-input" type="text" id="processNumber" name="processNumber" v-model="process.number"><br>
+
+        <label class="adble-label" for="jurisdiction">Jurisdição</label><br>
+        <input class="adble-input" type="text" id="jurisdiction" name="jurisdiction" v-model="process.jurisdiction"><br>
+
+        <!-- Restante do formulário aqui -->
+
+        <details class="adble-details">
+          <summary class="adble-summary">Partes no processo</summary>
+
+          <div class="adble-person" v-for="(person, index) in process.people" :key="index">
+            <span class="adble-name">
+              {{ person.nome }}
+              <small>{{ person.documento }}</small>
+            </span>
+            <span class="adble-badge">{{ person.polo }}</span>
+            <button class="adble-delete-button" @click="deletePerson(index)">Deletar</button>
+          </div>
+
+        </details>
+
+        <input class="adble-button" type="submit" value="Cadastrar">
+      </form>
+    </div>
 </template>
 <script>
 import { defineComponent, ref, onMounted } from "vue"; // Adicione 'computed' aqui
@@ -9,26 +33,124 @@ import { defineComponent, ref, onMounted } from "vue"; // Adicione 'computed' aq
 export default defineComponent({
   name: "processPage",
   setup() {
-    const htmlContent = ref("");
-    const textContent = ref("");
 
-    onMounted(() => {
-      chrome.runtime.sendMessage({ type: "GET_HTML_CONTENT" }, response => {
-        htmlContent.value = response;
-        const parser = new DOMParser();
-
-        const doc = parser.parseFromString(htmlContent.value, 'text/html');
-        const element = doc.querySelector('a.titulo-topo.dropdown-toggle.titulo-topo-desktop');
-
-        if (element) {
-          
-          textContent.value = element.textContent.trim();
-        }
-
-      });
+    const process = ref({
+      number: "",
+      jurisdiction: "",
+      people: []
+      // Adicione mais campos conforme necessário
     });
 
-    return { htmlContent, textContent };
+    onMounted(async () => {
+      const htmlContent = await new Promise(resolve => {
+        chrome.runtime.sendMessage({ type: "GET_HTML_CONTENT" }, response => {
+          resolve(response);
+        });
+      });
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(htmlContent, 'text/html');
+
+      process.value.number = getNumeroProcesso(doc);
+      process.value.jurisdiction = getOrgaoJulgador(doc);
+
+      const poloAtivo = getPoloAtivo(doc).map(person => ({ ...person, polo: 'Ativo' }));
+      const poloPassivo = getPoloPassivo(doc).map(person => ({ ...person, polo: 'Passivo' }));
+
+      process.value.people = [...poloAtivo, ...poloPassivo];
+    });
+
+    const deletePerson = (index) => {
+      process.value.people.splice(index, 1);
+    };
+
+    const getNumeroProcesso = (doc) => {
+      // O padrão procura uma sequência de dígitos seguida por um hífen, mais dígitos, um ponto, mais dígitos, um ponto, mais dígitos, um ponto e mais dígitos.
+      let padrao = /(\d{7}-\d{2}.\d{4}.\d{1}.\d{2}.\d{4})/;
+      let texto = doc.querySelector('.titulo-topo').textContent;
+      let resultado = padrao.exec(texto);
+
+      return resultado ? resultado[1] : 'Número do processo não encontrado';
+    };
+
+
+    const getAutuacao = (doc) => {
+      let padrao = /Autuação<\/dt>\s*<dd>(.*?)<\/dd>/;
+      let texto = doc.querySelector('#maisDetalhes').innerHTML;
+      let resultado = padrao.exec(texto);
+
+      return resultado ? resultado[1].trim() : 'Data de autuação não encontrada';
+    };
+
+    const getOrgaoJulgador = (doc) => {
+      let padrao = /Órgão julgador<\/dt>\s*<dd>(.*?)<\/dd>/;
+      let texto = doc.querySelector('#maisDetalhes').innerHTML;
+      let resultado = padrao.exec(texto);
+
+      return resultado ? resultado[1].trim() : 'Órgão julgador não encontrado';
+    };
+
+    const getPoloAtivo = (doc) => {
+      let elementosPoloAtivo = doc.querySelectorAll('#poloAtivo .table tbody tr td');
+      let poloAtivo = [];
+
+      elementosPoloAtivo.forEach(elemento => {
+        let texto = elemento.textContent;
+        let padrao = /(.+?)(?: - (CPF|CNPJ): (\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}|\d{3}\.\d{3}\.\d{3}-\d{2}))?(?=\s*\(|\s*$)/;
+        let resultado = padrao.exec(texto);
+
+        if (resultado) {
+          let documento = resultado[3] || 'Sem documento';
+          let tipo = resultado[2] || 'Sem documento';
+          poloAtivo.push({
+            nome: resultado[1].trim(),
+            documento: documento,
+            tipo: tipo
+          });
+        } else {
+          // Caso onde não há CPF ou CNPJ
+          poloAtivo.push({
+            nome: texto.trim(),
+            documento: 'Sem documento',
+            tipo: 'Sem documento'
+          });
+        }
+      });
+
+      return poloAtivo;
+    };
+
+    const getPoloPassivo = (doc) => {
+      let elementosPoloPassivo = doc.querySelectorAll('#poloPassivo .table tbody tr td');
+      let poloPassivo = [];
+
+      elementosPoloPassivo.forEach(elemento => {
+        let texto = elemento.textContent;
+        let padrao = /(.+?)(?: - (CPF|CNPJ): (\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}|\d{3}\.\d{3}\.\d{3}-\d{2}))?(?=\s*\(|\s*$)/;
+        let resultado = padrao.exec(texto);
+
+        if (resultado) {
+          let documento = resultado[3] || 'Sem documento';
+          let tipo = resultado[2] || 'Sem documento';
+          poloPassivo.push({
+            nome: resultado[1].trim(),
+            documento: documento,
+            tipo: tipo
+          });
+        } else {
+          // Caso onde não há CPF ou CNPJ
+          poloPassivo.push({
+            nome: texto.trim(),
+            documento: 'Sem documento',
+            tipo: 'Sem documento'
+          });
+        }
+      });
+
+      return poloPassivo;
+    };
+
+    return { deletePerson, process };
   }
 });
 
